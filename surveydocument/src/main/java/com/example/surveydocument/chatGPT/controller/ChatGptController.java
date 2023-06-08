37d1473eb58965eb551544a5f72547c7eca48435
Leesungsup <tmdtjq0116@naver.com>
2023-06-08 17:00:29 +0900

    Update ChatGptController.java

--- surveydocument/src/main/java/com/example/surveydocument/chatGPT/controller/ChatGptController.java
+++ surveydocument/src/main/java/com/example/surveydocument/chatGPT/controller/ChatGptController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/document/internal")
+@RequestMapping("/api/document/external")
 public class ChatGptController {
 
     private final ChatGptService chatGptService;
